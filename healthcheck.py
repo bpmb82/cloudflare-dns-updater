7f21a7e1148139d5d5e09f664662f4e50de50546
bpmb82 <56978424+bpmb82@users.noreply.github.com>
2021-01-13 09:42:07 +0100

    Update healthcheck.py

--- healthcheck.py
+++ healthcheck.py
@@ -17,7 +17,7 @@ age = now - mod
 # exit(1) if age of the file is higher than 3 times the timeout, else exit(0)
 if age > timeout:
     print(time.ctime(time.time()),'[ health error ] Healthcheck failed'
-	  exit(1)
+    exit(1)
 else:
     print(time.ctime(time.time()),f'[ health info ] Age of the healthfile was {age}')
     exit(0)
